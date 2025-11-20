c4250fc82b0192b6ead4c13e9b73efc4bd818773
The1Lord <64983657+The1Lord@users.noreply.github.com>
2025-11-20 16:42:48 +0000

    Update move_joint_test.py

--- move_joint_test.py
+++ move_joint_test.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
+# need this at the start of every python file so linux knows its python 
 import sys
 import rospy
-import moveit_commander
+import moveit_commander #import dependencies 
 
 def main():
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node("ur5_move_test", anonymous=True)
-
+#ur5_move_test is name of pyhton file (node) 
     group = moveit_commander.MoveGroupCommander("manipulator")
 
     rospy.loginfo("Moving joint 1 +0.2 rad...")
